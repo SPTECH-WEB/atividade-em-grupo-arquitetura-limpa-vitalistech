61483454debabf787a4a7d1acb45472eefbb3998
Igor <igor.jimenez@sptech.school>
2025-08-26 19:41:10 -0300

    Iniciando o projeto

---
+++ src/main/java/vitalis/tech/vitalis/VitalisApplication.java
@@ -0,0 +1,13 @@
+package vitalis.tech.vitalis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitalisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitalisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vitalis/tech/vitalis/VitalisApplicationTests.java
@@ -0,0 +1,13 @@
+package vitalis.tech.vitalis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitalisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
